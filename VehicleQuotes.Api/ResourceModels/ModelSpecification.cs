610b09a1d8f32f6eaa787fb00e81928077049b08
Rebecca Mutoni <rbccmtni1gmail.com>
2025-07-23 16:58:59 +0200

    add validations

--- VehicleQuotes.Api/ResourceModels/ModelSpecification.cs
+++ VehicleQuotes.Api/ResourceModels/ModelSpecification.cs
@@ -1,9 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace VehicleQuotes.Api.ResourceModels;
 
 public class ModelSpecification
 {
     public int ID { get; set; }
+    [Required]
     public string Name { get; set; }    
-    
+    [Required]
     public ModelSpecificationStyle[] Styles { get; set; }
 }
--- VehicleQuotes.Api/ResourceModels/ModelSpecificationStyle.cs
+++ VehicleQuotes.Api/ResourceModels/ModelSpecificationStyle.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace VehicleQuotes.Api.ResourceModels;
 
 public class ModelSpecificationStyle
 {
+    [Required]
     public string BodyType { get; set; }
+    [Required]
     public string Size { get; set; }   
-    
+    [Required]
+    [MinLength(1)]
     public string[] Years { get; set; }
 }
