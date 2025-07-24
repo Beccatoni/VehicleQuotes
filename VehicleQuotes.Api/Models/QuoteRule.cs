c1d481341e26917b587830cfb4e2be9dd4838ec8
Rebecca Mutoni <rbccmtni1gmail.com>
2025-07-24 08:16:48 +0200

    add the feature types

--- VehicleQuotes.Api/Models/QuoteRule.cs
+++ VehicleQuotes.Api/Models/QuoteRule.cs
@@ -1,6 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using VehicleQuotes.Api.Validation;
 namespace VehicleQuotes.Api.Models;
 
+[Index(nameof(FeatureType), nameof(FeatureValue), IsUnique = true)]
 public class QuoteRule
 {
-    
+    public static class FeatureTypes
+    {
+        public static string BodyType = "BodyType";
+        public static string Size = "Size";
+        public static string ItMoves = "ItMoves";
+        public static string HasAllWheels = "HasAllWheels";
+        public static string HasAlloyWheels = "HasAlloyWheels";
+        public static string HasAllTires = "HasAllTires";
+        public static string HasKey = "HasKey";
+        public static string HasTitle = "HasTitle";
+        public static string RequiresPickup = "RequiresPickup";
+        public static string HasEngine = "HasEngine";
+        public static string HasTransmission = "HasTransmission";
+        public static string HasCompleteInterior = "HasCompleteInterior";
+
+        public static string[] All => new string[]
+        {
+            BodyType, Size, ItMoves, HasAllWheels, HasAlloyWheels, HasAllTires, HasKey, HasTitle, RequiresPickup,
+            HasEngine, HasTransmission, HasCompleteInterior
+        };
+    }
+    public int ID { get; set; }
+    [FeatureType]
+    public string FeatureType { get; set; }
+    public string FeatureValue { get; set; }
+    public decimal PriceModifier { get; set; }
+   
 }
