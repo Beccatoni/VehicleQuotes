0b780777cbc82b41951b6484fa791eb9b7ba757b
Rebecca Mutoni <rbccmtni1@gmail.com>
2025-08-28 09:03:46 +0200

    leave the commented code for learning

--- VehicleQuotes.Api/Controllers/BodyTypesController.cs
+++ VehicleQuotes.Api/Controllers/BodyTypesController.cs
@@ -23,6 +23,97 @@ namespace VehicleQuotes.Api.Controllers
             return await _context.BodyTypes.ToListAsync();
         }
 
+        
+        /// <summary>
+        /// These endpoints are commented because we have seed data in the database, so we won't need them
+        /// for we have fixed data that we don't want to retriev, update or create anymore
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        // GET: api/BodyTypes/5
+        // [HttpGet("{id}")]
+        // public async Task<ActionResult<BodyType>> GetBodyType(int id)
+        // {
+        //     var bodyType = await _context.BodyTypes.FindAsync(id);
+        //
+        //     if (bodyType == null)
+        //     {
+        //         return NotFound();
+        //     }
+        //
+        //     return bodyType;
+        // }
+
+        // PUT: api/BodyTypes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        // [HttpPut("{id}")]
+        // public async Task<IActionResult> PutBodyType(int id, BodyType bodyType)
+        // {
+        //     if (id != bodyType.ID)
+        //     {
+        //         return BadRequest();
+        //     }
+        //
+        //     _context.Entry(bodyType).State = EntityState.Modified;
+        //
+        //     try
+        //     {
+        //         await _context.SaveChangesAsync();
+        //     }
+        //     catch (DbUpdateConcurrencyException)
+        //     {
+        //         if (!BodyTypeExists(id))
+        //         {
+        //             return NotFound();
+        //         }
+        //         else
+        //         {
+        //             throw;
+        //         }
+        //     }
+        //     catch (Microsoft.EntityFrameworkCore.DbUpdateException)
+        //     {
+        //         return Conflict();
+        //     }
+        //
+        //     return NoContent();
+        // }
+
+        // POST: api/BodyTypes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        // [HttpPost]
+        // public async Task<ActionResult<BodyType>> PostBodyType(BodyType bodyType)
+        // {
+        //     try
+        //     {
+        //         _context.BodyTypes.Add(bodyType);
+        //         await _context.SaveChangesAsync();
+        //     }
+        //     catch (Microsoft.EntityFrameworkCore.DbUpdateException)
+        //     {
+        //         return Conflict();
+        //     }
+        //     
+        //
+        //     return CreatedAtAction("GetBodyType", new { id = bodyType.ID }, bodyType);
+        // }
+
+        // DELETE: api/BodyTypes/5
+        // [HttpDelete("{id}")]
+        // public async Task<IActionResult> DeleteBodyType(int id)
+        // {
+        //     var bodyType = await _context.BodyTypes.FindAsync(id);
+        //     if (bodyType == null)
+        //     {
+        //         return NotFound();
+        //     }
+        //
+        //     _context.BodyTypes.Remove(bodyType);
+        //     await _context.SaveChangesAsync();
+        //
+        //     return NoContent();
+        // }
+
         private bool BodyTypeExists(int id)
         {
             return _context.BodyTypes.Any(e => e.ID == id);
--- VehicleQuotes.Api/Controllers/SizesController.cs
+++ VehicleQuotes.Api/Controllers/SizesController.cs
@@ -22,6 +22,96 @@ namespace VehicleQuotes.Api.Controllers
         {
             return await _context.Sizes.ToListAsync();
         }
-        
+
+        // These endpoints are commented because we have seed data in the database, so we won't need them'
+        // // GET: api/Sizes/5
+        // [HttpGet("{id}")]
+        // public async Task<ActionResult<Size>> GetSize(int id)
+        // {
+        //     var size = await _context.Sizes.FindAsync(id);
+        //
+        //     if (size == null)
+        //     {
+        //         return NotFound();
+        //     }
+        //
+        //     return size;
+        // }
+
+       
+        // // PUT: api/Sizes/5
+        // // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        // [HttpPut("{id}")]
+        // public async Task<IActionResult> PutSize(int id, Size size)
+        // {
+        //     if (id != size.ID)
+        //     {
+        //         return BadRequest();
+        //     }
+        //
+        //     _context.Entry(size).State = EntityState.Modified;
+        //
+        //     try
+        //     {
+        //         await _context.SaveChangesAsync();
+        //     }
+        //     catch (DbUpdateConcurrencyException)
+        //     {
+        //         if (!SizeExists(id))
+        //         {
+        //             return NotFound();
+        //         }
+        //         else
+        //         {
+        //             throw;
+        //         }
+        //     }
+        //     catch (Microsoft.EntityFrameworkCore.DbUpdateException)
+        //     {
+        //         return Conflict();
+        //     }
+        //
+        //     return NoContent();
+        // }
+
+        // // POST: api/Sizes
+        // // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        // [HttpPost]
+        // public async Task<ActionResult<Size>> PostSize(Size size)
+        // {
+        //     try
+        //     {
+        //         _context.Sizes.Add(size);
+        //         await _context.SaveChangesAsync();
+        //     }
+        //     catch (Microsoft.EntityFrameworkCore.DbUpdateException)
+        //     {
+        //         return Conflict();
+        //     }
+        //     
+        //
+        //     return CreatedAtAction("GetSize", new { id = size.ID }, size);
+        // }
+        //
+        // // DELETE: api/Sizes/5
+        // [HttpDelete("{id}")]
+        // public async Task<IActionResult> DeleteSize(int id)
+        // {
+        //     var size = await _context.Sizes.FindAsync(id);
+        //     if (size == null)
+        //     {
+        //         return NotFound();
+        //     }
+        //
+        //     _context.Sizes.Remove(size);
+        //     await _context.SaveChangesAsync();
+        //
+        //     return NoContent();
+        // }
+        //
+        // private bool SizeExists(int id)
+        // {
+        //     return _context.Sizes.Any(e => e.ID == id);
+        // }
     }
 }
